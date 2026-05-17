e02c6eb9c0b4886a81fdd4cd1e81919308ca802e
awatech <awatech12@gmail.com>
2026-05-17 14:20:46 +0100

    changes  made

--- myApp/settings.py
+++ myApp/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY") 
 DEBUG = os.getenv("DEBUG", 'False') == 'True'  
-USE_CLOUDINARY = False
+USE_CLOUDINARY = True
 
 # Allowed hosts configuration
 ALLOWED_HOSTS = [
